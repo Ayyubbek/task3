b57ee46b3f05777f6b0225b25e46fe0a383197e2
Ayyubbek <akhmadovayyubbek0131@gmail.com>
2025-09-23 11:45:25 +0500

    fix: use process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const url = require("url");
 
-// EKUK (LCM) hisoblash
 function gcd(a, b) {
   return b === 0 ? a : gcd(b, a % b);
 }
@@ -14,11 +13,11 @@ const server = http.createServer((req, res) => {
   const pathname = parsedUrl.pathname;
   const query = parsedUrl.query;
 
+  res.setHeader("Content-Type", "text/plain");
+
   if (pathname === "/akhmadovayyubbek0131_gmail_com") {
     const x = parseInt(query.x);
     const y = parseInt(query.y);
-
-    res.setHeader("Content-Type", "text/plain"); 
 
     if (Number.isNaN(x) || Number.isNaN(y) || x <= 0 || y <= 0) {
       res.end("NaN");
@@ -27,11 +26,12 @@ const server = http.createServer((req, res) => {
     }
   } else {
     res.statusCode = 404;
-    res.setHeader("Content-Type", "text/plain");
     res.end("Not found");
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000/");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
