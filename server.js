a25ea8d33093099232332efacd02b458e3c4ebde
Ayyubbek <akhmadovayyubbek0131@gmail.com>
2025-09-24 12:01:07 +0500

    fixing

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ function gcd(a, b) {
 }
 
 function lcm(a, b) {
-  if (a === 0n) return b;  
-  if (b === 0n) return a;
+  if (a === 0n || b === 0n) return 0n;
   return (a * b) / gcd(a, b);
 }
 
@@ -16,18 +15,18 @@ const server = http.createServer((req, res) => {
   const pathname = parsedUrl.pathname;
   const query = parsedUrl.query;
 
-  if (pathname === "/akhmadovayyubbek0131_gmail_com") {
+  if (pathname.includes("akhmadovayyubbek0131_gmail_com")) {
     try {
       const x = BigInt(query.x);
       const y = BigInt(query.y);
 
       if (x < 0n || y < 0n) {
-        res.end("0"); 
+        res.end("NaN");
       } else {
         res.end(lcm(x, y).toString());
       }
     } catch (e) {
-      res.end("0");
+      res.end("NaN");
     }
   } else {
     res.statusCode = 404;
